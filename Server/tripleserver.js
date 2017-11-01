323821f3bcf7c05fda948d1ab505a3c7089c822e
orim65 <33276455+orim65@users.noreply.github.com>
2017-11-01 16:57:27 +0700

    Update tripleserver.js

--- Server/tripleserver.js
+++ Server/tripleserver.js
@@ -139,5 +139,5 @@ app.post('/showmymate', cors(), function (req, res) {
 var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Who's your mate? - listening at http://%s:%s", host, port)
 });
